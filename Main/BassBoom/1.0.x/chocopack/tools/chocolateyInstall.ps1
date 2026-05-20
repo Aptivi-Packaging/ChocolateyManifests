4a5d851e0ed879ebb569bc4ac353e81809359cfd
Aptivi CEO <eoflaoevicecity@gmail.com>
2026-05-20 12:35:45 +0300

    pkg - Used WiX installer for Chocolatey (BassBoom)
    ---
    
    Type: pkg
    Breaking: No
    Doc Required: No
    Backport Required: No
    AI Assisted: No
    Part: 1/1

--- Main/BassBoom/1.0.x/chocopack/tools/chocolateyInstall.ps1
+++ Main/BassBoom/1.0.x/chocopack/tools/chocolateyInstall.ps1
@@ -1,13 +1,45 @@
 $ErrorActionPreference = 'Stop';
+Write-Output "<*>: for assumptions, <+> for progress, <-> for error"
+
+# Prepare the basic variables
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $pkgName    = "bassboom"
-$url        = "https://github.com/Aptivi/BassBoom/releases/download/v1.0.0/1.0.0-bin.zip"
-$shacheck   = "DAFFA4D562B606C660369628BFC162893057967A6864C4459BEBC0B2704E2309"
-
-Write-Output "<*>: for assumptions, <+> for progress, <-> for error"
+$version    = "v1.0.0"
 Write-Output "<*> Installation directory: $toolsDir"
-Write-Output "<*> Package Name: $pkgName"
+Write-Output "<*> Package Name: $pkgName ($version)"
+
+# Check the system architecture
+$architecture = [System.Runtime.InteropServices.RuntimeInformation]::OSArchitecture
+$arch         = switch ($architecture) {
+    "X64"   { "x64" }
+    "Arm64" { "arm64" }
+    Default { "unknown" }
+}
+Write-Output "<*> Architecture: $arch [$architecture]"
+if ($arch -eq "unknown") {
+    Throw "<-> BassBoom doesn't support $architecture"
+}
+
+# Determine the URL and the SHA256 sum
+$url        = "https://github.com/Aptivi/BassBoom/releases/download/$version/bassboom-win-$arch-installer.exe"
+$shacheck   = switch ($arch) {
+    "x64"   { "4E6DB3C15BCE120137B48400DB79B3E0CD8B327290151F933C8497A771F2C959" }
+    "arm64" { "265A26C574D8E6E9F748E949FB4975FCF9C2BBD8A34B90D5E86601ED5C36418B" }
+}
 Write-Output "<*> URL: $url"
 Write-Output "<*> Expected SHA256 Sum: $shacheck"
 
-Install-ChocolateyZipPackage $pkgName $url $toolsDir -ChecksumType "sha256" -Checksum $shacheck
+$packageArgs = @{
+  packageName   = $packageName
+  fileType      = 'exe'
+  url           = $url
+  silentArgs    = "/quiet /norestart"
+  validExitCodes= @(0, 3010, 1641)
+  softwareName  = 'BassBoom*'
+  checksum      = $shacheck
+  checksumType  = 'sha256'
+}
+
+Write-Output "<+> Starting installation..."
+Install-ChocolateyPackage @packageArgs
+Write-Output "<+> Installation complete!"
