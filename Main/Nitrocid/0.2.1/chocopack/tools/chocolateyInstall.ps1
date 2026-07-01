5b5156cae9fabbd1995e760499f32971f0e68e5b
Aptivi CEO <eoflaoevicecity@gmail.com>
2026-07-01 19:33:21 +0300

    pkg - Added NKS 0.2.1 Beta
    ---
    
    Type: pkg
    Breaking: No
    Doc Required: No
    Backport Required: No
    AI Assisted: No
    Part: 1/1

---
+++ Main/Nitrocid/0.2.1/chocopack/tools/chocolateyInstall.ps1
@@ -0,0 +1,42 @@
+$ErrorActionPreference = 'Stop';
+Write-Output "<*>: for assumptions, <+> for progress, <-> for error"
+
+# Prepare the basic variables
+$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$pkgName    = "KS"
+$version    = "v0.2.1.0-beta"
+Write-Output "<*> Installation directory: $toolsDir"
+Write-Output "<*> Package Name: $pkgName ($version)"
+
+# Check the system architecture
+$architecture = [System.Runtime.InteropServices.RuntimeInformation]::OSArchitecture
+Write-Output "<*> Architecture: $architecture"
+
+$packageArgs = @{
+  packageName   = $pkgName
+  fileType      = 'exe'
+  url           = "https://github.com/Aptivi/Nitrocid/releases/download/$version/nitrocid-win-x64-installer.exe"
+  url64bit      = "https://github.com/Aptivi/Nitrocid/releases/download/$version/nitrocid-win-x64-installer.exe"
+  silentArgs    = "/quiet /norestart"
+  validExitCodes= @(0, 3010, 1641)
+  softwareName  = 'Nitrocid*'
+  checksum      = "98657E3701E27BA3E8EB96B83D82A8ED01F2FE0378E7FE5BE36C9C4894AA3548"
+  checksumType  = 'sha256'
+  checksum64    = "98657E3701E27BA3E8EB96B83D82A8ED01F2FE0378E7FE5BE36C9C4894AA3548"
+  checksumType64= 'sha256'
+}
+
+# Change URL if ARM64 is detected
+if ($architecture -eq "Arm64") {
+    $packageArgs.url        = "https://github.com/Aptivi/Nitrocid/releases/download/$version/nitrocid-win-arm64-installer.exe"
+    $packageArgs.url64bit   = "https://github.com/Aptivi/Nitrocid/releases/download/$version/nitrocid-win-arm64-installer.exe"
+    $packageArgs.checksum   = "6D7CEFAA3717F5560933E5E0D8B9846A53A98761F2025F4CD0A1C44CA3CC209E"
+    $packageArgs.checksum64 = "6D7CEFAA3717F5560933E5E0D8B9846A53A98761F2025F4CD0A1C44CA3CC209E"
+}
+
+Write-Output "<*> URL: $($packageArgs.url)"
+Write-Output "<*> Expected SHA256 Sum: $($packageArgs.checksum)"
+
+Write-Output "<+> Starting installation..."
+Install-ChocolateyPackage @packageArgs
+Write-Output "<+> Installation complete!"
