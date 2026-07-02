3c71b6edbb2c8b774ff54320633618e38283678b
Aptivi CEO <eoflaoevicecity@gmail.com>
2026-07-02 12:47:35 +0300

    pkg - Resume distribution for older verisons!
    ---
    
    Type: pkg
    Breaking: No
    Doc Required: No
    Backport Required: No
    AI Assisted: No
    Part: 1/1

--- Main/Nitrocid/0.1.0/chocopack-lite/tools/chocolateyInstall.ps1
+++ Main/Nitrocid/0.1.0/chocopack-lite/tools/chocolateyInstall.ps1
@@ -10,35 +10,32 @@ Write-Output "<*> Package Name: $pkgName ($version)"
 
 # Check the system architecture
 $architecture = [System.Runtime.InteropServices.RuntimeInformation]::OSArchitecture
-$arch         = switch ($architecture) {
-    "X64"   { "x64" }
-    "Arm64" { "arm64" }
-    Default { "unknown" }
-}
-Write-Output "<*> Architecture: $arch [$architecture]"
-if ($arch -eq "unknown") {
-    Throw "<-> Nitrocid doesn't support $architecture"
-}
-
-# Determine the URL and the SHA256 sum
-$url        = "https://github.com/Aptivi/Nitrocid/releases/download/$version/nitrocid-win-$arch-installer.exe"
-$shacheck   = switch ($arch) {
-    "x64"   { "714CE358B33010DF70CDAB7A4ED27BA367FD4A00B2C40D80EE2CCC60821FB963" }
-    "arm64" { "9FB5235C6E9D40DEA9998374C9218ABBC67DD5E7FC64965A0FA8C576AEBAF790" }
-}
-Write-Output "<*> URL: $url"
-Write-Output "<*> Expected SHA256 Sum: $shacheck"
+Write-Output "<*> Architecture: $architecture"
 
 $packageArgs = @{
-  packageName   = $packageName
+  packageName   = $pkgName
   fileType      = 'exe'
-  url           = $url
+  url           = "https://github.com/Aptivi/Nitrocid/releases/download/$version/nitrocid-win-x64-installer.exe"
+  url64bit      = "https://github.com/Aptivi/Nitrocid/releases/download/$version/nitrocid-win-x64-installer.exe"
   silentArgs    = "/quiet /norestart NitrocidLite=1"
   validExitCodes= @(0, 3010, 1641)
   softwareName  = 'Nitrocid*'
-  checksum      = $shacheck
+  checksum      = "714CE358B33010DF70CDAB7A4ED27BA367FD4A00B2C40D80EE2CCC60821FB963"
   checksumType  = 'sha256'
+  checksum64    = "714CE358B33010DF70CDAB7A4ED27BA367FD4A00B2C40D80EE2CCC60821FB963"
+  checksumType64= 'sha256'
 }
+
+# Change URL if ARM64 is detected
+if ($architecture -eq "Arm64") {
+    $packageArgs.url        = "https://github.com/Aptivi/Nitrocid/releases/download/$version/nitrocid-win-arm64-installer.exe"
+    $packageArgs.url64bit   = "https://github.com/Aptivi/Nitrocid/releases/download/$version/nitrocid-win-arm64-installer.exe"
+    $packageArgs.checksum   = "9FB5235C6E9D40DEA9998374C9218ABBC67DD5E7FC64965A0FA8C576AEBAF790"
+    $packageArgs.checksum64 = "9FB5235C6E9D40DEA9998374C9218ABBC67DD5E7FC64965A0FA8C576AEBAF790"
+}
+
+Write-Output "<*> URL: $($packageArgs.url)"
+Write-Output "<*> Expected SHA256 Sum: $($packageArgs.checksum)"
 
 Write-Output "<+> Starting installation..."
 Install-ChocolateyPackage @packageArgs
--- Main/Nitrocid/0.2.0/chocopack-lite/tools/chocolateyInstall.ps1
+++ Main/Nitrocid/0.2.0/chocopack-lite/tools/chocolateyInstall.ps1
@@ -10,35 +10,32 @@ Write-Output "<*> Package Name: $pkgName ($version)"
 
 # Check the system architecture
 $architecture = [System.Runtime.InteropServices.RuntimeInformation]::OSArchitecture
-$arch         = switch ($architecture) {
-    "X64"   { "x64" }
-    "Arm64" { "arm64" }
-    Default { "unknown" }
-}
-Write-Output "<*> Architecture: $arch [$architecture]"
-if ($arch -eq "unknown") {
-    Throw "<-> Nitrocid doesn't support $architecture"
-}
-
-# Determine the URL and the SHA256 sum
-$url        = "https://github.com/Aptivi/Nitrocid/releases/download/$version/nitrocid-win-$arch-installer.exe"
-$shacheck   = switch ($arch) {
-    "x64"   { "7928D7B0AA043B3CF965DD796410F3F48B7660837D205101AB54D91A73364094" }
-    "arm64" { "4D9DC68B47CEA85D8AF523CAE1186BA257BB43B8E35C18761350F37EB65D504C" }
-}
-Write-Output "<*> URL: $url"
-Write-Output "<*> Expected SHA256 Sum: $shacheck"
+Write-Output "<*> Architecture: $architecture"
 
 $packageArgs = @{
-  packageName   = $packageName
+  packageName   = $pkgName
   fileType      = 'exe'
-  url           = $url
+  url           = "https://github.com/Aptivi/Nitrocid/releases/download/$version/nitrocid-win-x64-installer.exe"
+  url64bit      = "https://github.com/Aptivi/Nitrocid/releases/download/$version/nitrocid-win-x64-installer.exe"
   silentArgs    = "/quiet /norestart NitrocidLite=1"
   validExitCodes= @(0, 3010, 1641)
   softwareName  = 'Nitrocid*'
-  checksum      = $shacheck
+  checksum      = "7928D7B0AA043B3CF965DD796410F3F48B7660837D205101AB54D91A73364094"
   checksumType  = 'sha256'
+  checksum64    = "7928D7B0AA043B3CF965DD796410F3F48B7660837D205101AB54D91A73364094"
+  checksumType64= 'sha256'
 }
+
+# Change URL if ARM64 is detected
+if ($architecture -eq "Arm64") {
+    $packageArgs.url        = "https://github.com/Aptivi/Nitrocid/releases/download/$version/nitrocid-win-arm64-installer.exe"
+    $packageArgs.url64bit   = "https://github.com/Aptivi/Nitrocid/releases/download/$version/nitrocid-win-arm64-installer.exe"
+    $packageArgs.checksum   = "4D9DC68B47CEA85D8AF523CAE1186BA257BB43B8E35C18761350F37EB65D504C"
+    $packageArgs.checksum64 = "4D9DC68B47CEA85D8AF523CAE1186BA257BB43B8E35C18761350F37EB65D504C"
+}
+
+Write-Output "<*> URL: $($packageArgs.url)"
+Write-Output "<*> Expected SHA256 Sum: $($packageArgs.checksum)"
 
 Write-Output "<+> Starting installation..."
 Install-ChocolateyPackage @packageArgs
