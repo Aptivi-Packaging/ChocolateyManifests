9f1cb53dfd7da876e7d2aadbc0b1d5277adc0f36
Aptivi CEO <eoflaoevicecity@gmail.com>
2026-08-27 19:15:49 +0300

    add - Added BB 1.0.2.2
    ---
    
    We've added packages for BassBoom v1.0.2.2
    
    ---
    
    Type: add
    Breaking: No
    Doc Required: No
    Backport Required: No
    AI Assisted: No
    Part: 1/1

--- Main/BassBoom/1.0.x/chocopack/tools/chocolateyInstall.ps1
+++ Main/BassBoom/1.0.x/chocopack/tools/chocolateyInstall.ps1
@@ -4,7 +4,7 @@ Write-Output "<*>: for assumptions, <+> for progress, <-> for error"
 # Prepare the basic variables
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $pkgName    = "bassboom"
-$version    = "v1.0.2"
+$version    = "v1.0.2.2"
 Write-Output "<*> Installation directory: $toolsDir"
 Write-Output "<*> Package Name: $pkgName ($version)"
 
@@ -20,9 +20,9 @@ $packageArgs = @{
   silentArgs    = "/quiet /norestart"
   validExitCodes= @(0, 3010, 1641)
   softwareName  = 'Nitrocid*'
-  checksum      = "863D519A296D21DF357366862C30E169199B7972A94182A82CF90FB3B21C9567"
+  checksum      = "539D4629BB623060A9F70D5730032E45778B860836A398912D42867C17A94046"
   checksumType  = 'sha256'
-  checksum64    = "863D519A296D21DF357366862C30E169199B7972A94182A82CF90FB3B21C9567"
+  checksum64    = "539D4629BB623060A9F70D5730032E45778B860836A398912D42867C17A94046"
   checksumType64= 'sha256'
 }
 
@@ -30,8 +30,8 @@ $packageArgs = @{
 if ($architecture -eq "Arm64") {
     $packageArgs.url        = "https://github.com/Aptivi/BassBoom/releases/download/$version/bassboom-win-arm64-installer.exe"
     $packageArgs.url64bit   = "https://github.com/Aptivi/BassBoom/releases/download/$version/bassboom-win-arm64-installer.exe"
-    $packageArgs.checksum   = "AAA817752DED77A5558BB0A869DEEE434BC2BE12154635452BAC597B61A79F02"
-    $packageArgs.checksum64 = "AAA817752DED77A5558BB0A869DEEE434BC2BE12154635452BAC597B61A79F02"
+    $packageArgs.checksum   = "2E74AD01E7E0FA0FCD2D5F8D746B2B2673EF88B11F0900104AB61550743C2120"
+    $packageArgs.checksum64 = "2E74AD01E7E0FA0FCD2D5F8D746B2B2673EF88B11F0900104AB61550743C2120"
 }
 
 Write-Output "<*> URL: $($packageArgs.url)"
